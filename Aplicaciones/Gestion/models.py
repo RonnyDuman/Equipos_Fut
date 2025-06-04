5ec3169f92de7eb0d4d020a3cba5b7ecaa6adec7
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-06-04 14:53:26 -0500

    Creamos el modelo jugador

--- Aplicaciones/Gestion/models.py
+++ Aplicaciones/Gestion/models.py
@@ -13,3 +13,18 @@ class Equipo(models.Model):
     def _str_(self):
         fila = "{0}: {1} - {2} - {3}"
         return fila.format(self.id, self.nombre, self.ciudad, self.entrenador)
+    
+
+# Modelo Jugador
+class Jugador(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    posicion = models.CharField(max_length=100)
+    numero_camiseta = models.IntegerField()
+    foto = models.ImageField(upload_to='jugadores/fotos/', null=True, blank=True)
+    ficha_pdf = models.FileField(upload_to='jugadores/pdfs/', null=True, blank=True)
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+
+    def _str_(self):
+        fila = "{0}: {1} - {2} - {3} - {4}"
+        return fila.format(self.id, self.nombre, self.posicion, self.numero_camiseta, self.equipo)
