06766c10cf539694f712e749fcde72bf524adaa5
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-06-04 16:54:57 -0500

    Creamos las rutas para Jugador

--- Aplicaciones/Gestion/urls.py
+++ Aplicaciones/Gestion/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    # Rutas para EQUIPOS
+    # Rutas para Equipos
     path('equipos/', views.listarEquipos, name="listar_equipos"),
     path('equipos/nuevo/', views.nuevoEquipo, name="nuevo_equipo"),
     path('equipos/guardar/', views.guardarEquipo, name="guardar_equipo"),
@@ -10,6 +10,13 @@ urlpatterns = [
     path('equipos/actualizar/', views.procesarEdicionEquipo, name="actualizar_equipo"),
     path('equipos/eliminar/<int:id>/', views.eliminarEquipo, name="eliminar_equipo"),
 
+    # Rutas para Jugadores
+    path('jugadores/<int:id_equipo>/', views.listarJugadoresPorEquipo, name="listar_jugadores"),
+    path('jugadores/nuevo/<int:id_equipo>/', views.nuevoJugador, name="nuevo_jugador"),
+    path('jugadores/guardar/', views.guardarJugador, name="guardar_jugador"),
+    path('jugadores/editar/<int:id>/', views.editarJugador, name="editar_jugador"),
+    path('jugadores/actualizar/', views.procesarEdicionJugador, name="actualizar_jugador"),
+    path('jugadores/eliminar/<int:id>/', views.eliminarJugador, name="eliminar_jugador"),
 
 ]
 
